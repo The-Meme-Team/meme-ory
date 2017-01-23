9add7b9c3375b714610ba3b46d5724adab531060
Morgan Nomura <morganelle@gmail.com>
2017-01-23 12:03:28 -0800

    initial object creation

---
+++ game.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function Meme(id, name) {
+  this.id = id;
+  this.name = name;
+}
+Meme.prototype.makeCard = function() {
+  // boilerplate
+};
+
+var meme1 = new Meme(1, meme1);
+var meme2 = new Meme(2, meme2);
+var meme3 = new Meme(3, meme3);
+var meme4 = new Meme(4, meme4);
+var meme5 = new Meme(5, meme1);
+var meme6 = new Meme(6, meme2);
+var meme7 = new Meme(7, meme1);
+var meme8 = new Meme(8, meme8);
